cada78076e98749037e83d673995ab2382e1a893
Pablo Laso Cortabitarte <31635267+plaso@users.noreply.github.com>
2021-07-10 09:59:13 +0200

    Project first days

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,33 @@
 const router = require('express').Router();
+const passport = require('passport')
 const miscController = require('../controllers/misc.controller');
+const authController = require('../controllers/auth.controller');
+const usersController = require('../controllers/users.controller');
+const upload = require('./storage.config')
+
+const authMiddleware = require('../middlewares/auth.middleware');
+
+const GOOGLE_SCOPES = [
+  'https://www.googleapis.com/auth/userinfo.email',
+  'https://www.googleapis.com/auth/userinfo.profile'
+]
 
 router.get('/', miscController.index);
+
+// Register
+router.get('/register', authMiddleware.isNotAuthenticated, authController.register);
+router.post('/register', upload.single("image"), authController.doRegister);
+
+// Login
+router.get('/login', authMiddleware.isNotAuthenticated, authController.login);
+router.post('/login', authMiddleware.isNotAuthenticated, authController.doLogin);
+router.get('/auth/google', authMiddleware.isNotAuthenticated, passport.authenticate('google-auth', { scope: GOOGLE_SCOPES }))
+router.get('/auth/google/callback', authMiddleware.isNotAuthenticated, authController.doLoginGoogle)
+router.post('/logout', authMiddleware.isAuthenticated, authController.logout)
+
+router.get('/activate/:token', authMiddleware.isNotAuthenticated, authController.activateAccount)
+
+// Profile
+router.get('/profile', authMiddleware.isAuthenticated, usersController.profile)
 
 module.exports = router;
